e20587765911823eebb1a0d79a26fbaecf64ed98
Christian Lopes <christianmiguellopes@gmail.com>
2026-04-19 19:08:38 -0300

    Adicionando a Classe D

---
+++ classeD.py
@@ -0,0 +1,11 @@
+class D:
+    def __init__(self):
+        # Atributos de sua escolha (exemplo: int e string)
+        self.D1 = 0
+        self.D2 = ""
+
+    def MD1(self):
+        print("Método MD1")
+
+    def MD2(self):
+        print("Método MD2")
